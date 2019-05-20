44aa60139ce40395e83ae344e37f6dc0a66996d6
Jack Pearsall <jack.pearsall@visiosound.co.uk>
2019-05-21 00:12:33 +0100

    Refactored into routes and controllers up to numbers, tests passing

--- src/app.js
+++ src/app.js
@@ -1,19 +1,7 @@
 const express = require('express');
-const {
-  sayHello,
-  uppercase,
-  lowercase,
-  firstCharacter,
-  firstCharacters,
-} = require('./lib/strings');
+const stringsRouter = require('./routes/strings');
+const numbersRouter = require('./routes/numbers');
 
-const {
-  add,
-  subtract,
-  multiply,
-  divide,
-  remainder,
-} = require('./lib/numbers');
 
 const {
   negate,
@@ -34,85 +22,9 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/strings/hello/:string', (req, res) => {
-  res.status(200).json({ result: sayHello(req.params.string) });
-});
+app.use('/strings', stringsRouter);
 
-app.get('/strings/upper/:string', (req, res) => {
-  res.status(200).json({ result: uppercase(req.params.string) });
-});
-
-app.get('/strings/lower/:string', (req, res) => {
-  res.status(200).json({ result: lowercase(req.params.string) });
-});
-
-app.get('/strings/first-character/:string', (req, res) => {
-  res.status(200).json({ result: firstCharacter(req.params.string) });
-});
-
-app.get('/strings/first-characters/:string', (req, res) => {
-  res.status(200).json({ result: firstCharacters(req.params.string, req.query.length) });
-});
-
-app.get('/numbers/add/:a/and/:b', (req, res) => {
-  const { a, b } = req.params;
-  if (isNaN(a) && isNaN(b)) {
-    res.status(400).json({ error: 'Parameters must be valid numbers.' });
-  } else {
-    res.status(200).json({ result: add(parseInt(a), parseInt(b)) });
-  }
-});
-
-app.get('/numbers/subtract/:a/from/:b', (req, res) => {
-  const { a, b } = req.params;
-  if (isNaN(a) && isNaN(b)) {
-    res.status(400).json({ error: 'Parameters must be valid numbers.' });
-  } else {
-    res.status(200).json({ result: subtract(parseInt(b), parseInt(a)) });
-  }
-});
-
-app.post('/numbers/multiply', (req, res) => {
-  const { a, b } = req.body;
-  if (a === undefined || b === undefined) {
-    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
-  } else if (isNaN(a) && isNaN(b)) {
-    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
-  } else {
-    res.status(200).json({ result: multiply(a, b) });
-  }
-});
-
-app.post('/numbers/divide', (req, res) => {
-  const { a, b } = req.body;
-  if (a === undefined || b === undefined) {
-    res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
-  } else if (isNaN(a) || isNaN(b)) {
-    res.status(400).json({ error: 'Parameters "a" and "b" must be valid numbers.' });
-  } else if (b === 0) {
-    res.status(400).json({ error: 'Unable to divide by 0.' });
-  } else {
-    res.status(200).json({ result: divide(a, b) });
-  }
-});
-
-app.post('/numbers/remainder', (req, res) => {
-  if (req.query.a && req.query.a) {
-    const { a, b } = req.query;
-    res.status(200).json({ result: remainder(a, b) });
-  } else {
-    const { a, b } = req.body;
-    if (a === undefined || b === undefined) {
-      res.status(400).json({ error: 'Parameters "a" and "b" are required.' });
-    } else if (isNaN(a) || isNaN(b)) {
-      res.status(400).json({ error: 'Parameters must be valid numbers.' });
-    } else if (b === 0) {
-      res.status(400).json({ error: 'Unable to divide by 0.' });
-    } else {
-      res.status(200).json({ result: remainder(a, b) });
-    }
-  }
-});
+app.use('/numbers', numbersRouter);
 
 app.post('/booleans/negate', (req, res) => {
   res.status(200).json({ result: negate(req.body.value) });
---
+++ src/routes/numbers.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const numbersController = require('../controllers/numbers');
+
+
+const router = express.Router();
+
+router.get('/add/:a/and/:b', numbersController.add);
+
+router.get('/subtract/:a/from/:b', numbersController.subtract);
+
+router.post('/multiply', numbersController.multiply);
+
+router.post('/divide', numbersController.divide);
+
+router.post('/remainder', numbersController.remainder);
+
+
+module.exports = router;
